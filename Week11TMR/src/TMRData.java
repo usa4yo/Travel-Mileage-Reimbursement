5e8d06211c969b75976c114dc54d848d504b0f80
ar25 <ar25@AA-28QFMF2.A70ADOM.bcbssc.com>
2017-04-12 17:30:30 -0400

    Coded more stuff...

--- Week11TMR/src/TMRData.java
+++ Week11TMR/src/TMRData.java
@@ -1,3 +1,5 @@
+
+
 /**
  * Class        : TMRData
  * Project 12   : Travel Mileage Reimbursement
@@ -7,6 +9,70 @@
  * Last Mod.    : 2017-04-11
  * Due Date     : 2017-04-12
  */
-public class TMRData {
 
+public class TMRData {
+    private static int[] [] mileage =   { {0, 1004, 1753, 2752, 3017, 1520, 1507, 609, 3155, 448},
+            {1004, 0, 921, 1780, 2048, 1397, 919, 515, 2176, 709},
+            {1753, 921, 0, 1230, 1399, 1343, 517, 1435, 2234, 1307},
+            {2752, 1780, 1230, 0, 272, 2570, 1732, 2251, 1322, 2420},
+            {3017, 2048, 1399, 272, 0, 2716, 1858, 2523, 1278, 2646},
+            {1520, 1397, 1343, 2570, 2716, 0, 860, 1494, 3447, 1057},
+            {1507, 919, 517, 1732, 1858, 860, 0, 1307, 2734, 1099},
+            {609, 515, 1435, 2251, 2523, 1494, 1307, 0, 2820, 571},
+            {3155, 2176, 2234, 1322, 1278, 3447, 2734, 2820, 0, 2887},
+            {448, 709, 1307, 2420, 2646, 1057, 1099, 571, 2887, 0}
+
+        };
+
+    private int numberOfCity;
+    private String city;
+ 
+    public TMRData(int numberOfCity, String city) {
+        super();
+        this.numberOfCity = numberOfCity;
+        this.city = city;
+    }
+
+    /**
+     * @return the mileage
+     */
+    public static int[][] getMileage() {
+        return mileage;
+    }   // End bracket of getMileage method
+
+    /**
+     * @param mileage the mileage to set
+     */
+    public static void setMileage(int[][] mileage) {
+        TMRData.mileage = mileage;
+    }   // End bracket of setMileage method
+
+    /**
+     * @return the numberOfCity
+     */
+    public int getNumberOfCity() {
+        return numberOfCity;
+    }   // End bracket of getNumberOfCity method
+
+    /**
+     * @param numberOfCity the numberOfCity to set
+     */
+    public void setNumberOfCity(int numberOfCity) {
+        this.numberOfCity = numberOfCity;
+    }   // End bracket of setNumberOfCity method
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }   // End bracket of getCity method
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }   // End bracket of setCity method
+   
 }	// End bracket of TMRData class
--- Week11TMR/src/TMRTextView.java
+++ Week11TMR/src/TMRTextView.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Class        : TMRTextView
  * Project 12   : Travel Mileage Reimbursement
@@ -8,6 +10,34 @@
  * Due Date     : 2017-04-12
  */
 
+
 public class TMRTextView {
 
-}	// End bracket of TMRTextView class
+    public static int[] cities;
+    
+    private static final String NUMBER_OF_CITY_VISITED = "Please enter the number of city your have visited";
+    private static final String REQUEST_STARTING_CITY = "Please enter the starting city";
+    private static final String REQUEST_NEXT_CITY = "Next city";    
+        
+    // Define keyboard as a scanner
+    static Scanner kb;
+    
+    public static void inputCities(int[] input){
+        kb = new Scanner(System.in);
+
+        System.out.println(NUMBER_OF_CITY_VISITED);
+        cities = new int[kb.nextInt()];
+        
+        System.out.println(REQUEST_STARTING_CITY);
+        for(int i = 0; i < input.length; ++i){
+            System.out.print(REQUEST_NEXT_CITY + (i+1) + ": " );
+            input[i] = kb.nextInt();
+        }
+    
+        kb.close();
+
+    }   // End of bracket inputCity method
+
+}   // End bracket of TMRTextView class
+    
+
